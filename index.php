6f548470dc4328a9cbbc9ff2f97a4da6211324c2
Pmnmpr <pmnmpr@gmail.com>
2022-01-04 15:32:00 +0000

    Obfuscated

---
+++ index.php
@@ -0,0 +1,7 @@
+<?php
+/*   _______________________________________
+    |  Obfuscated by PMPR - Php Obfuscator  |
+    |             61d46840eec00             |
+    |_______________________________________|
+*/
+ use Pmpr\Package\AMP\AMP; AMP::symcgieuakksimmu(); if (!function_exists("\151\163\x5f\x61\x6d\x70")) { function is_amp() { return AMP::symcgieuakksimmu()->oywyqcgumoecwoga(); } } if (!function_exists("\x69\x73\137\x61\155\x70\137\x65\x6e\144\160\x6f\151\156\x74")) { function is_amp_endpoint() { return AMP::symcgieuakksimmu()->smowememmgeukwki(); } }
