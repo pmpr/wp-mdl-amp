7b233d2433e9e7dc4c400cebf2e9dfeaf6fd57b4
Pmnmpr <pmnmpr@gmail.com>
2022-01-24 16:21:57 +0000

    Obfuscated

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php
 /*   _______________________________________
     |  Obfuscated by PMPR - Php Obfuscator  |
-    |             61d48ad3cebba             |
+    |             61eed202bc0dd             |
     |_______________________________________|
 */
- use Pmpr\Package\AMP\AMP; AMP::symcgieuakksimmu(); if (!function_exists("\x69\x73\x5f\x61\x6d\160")) { function is_amp() { return AMP::symcgieuakksimmu()->oywyqcgumoecwoga(); } } if (!function_exists("\151\163\x5f\x61\x6d\x70\x5f\x65\156\x64\x70\157\x69\x6e\x74")) { function is_amp_endpoint() { return AMP::symcgieuakksimmu()->smowememmgeukwki(); } }
+ use Pmpr\Package\AMP\AMP; AMP::symcgieuakksimmu(); if (!function_exists("\x69\x73\x5f\x61\x6d\x70")) { function is_amp() { return AMP::symcgieuakksimmu()->oywyqcgumoecwoga(); } } if (!function_exists("\151\x73\x5f\141\155\160\137\145\x6e\x64\x70\157\x69\156\164")) { function is_amp_endpoint() { return AMP::symcgieuakksimmu()->smowememmgeukwki(); } }
--- template/php/header.php
+++ template/php/header.php
@@ -1,16 +1,16 @@
 <?php
 /*   _______________________________________
     |  Obfuscated by PMPR - Php Obfuscator  |
-    |             61d48ad3cebba             |
+    |             61eed202bc0dd             |
     |_______________________________________|
 */
 ype html>
 <html amp ⚡ <?php  language_attributes(); ?>>
 <head>
-	<meta charset="<?php  bloginfo("\143\150\x61\x72\x73\145\x74"); ?>" />
+	<meta charset="<?php  bloginfo("\143\x68\141\162\x73\145\164"); ?>" />
 	<meta name="viewport" content="width=device-width, initial-scale=1" />
 	<link rel="profile" href="https://gmpg.org/xfn/11" />
-	<?php  do_action("\141\x6d\x70\137\x77\160\x5f\150\145\x61\x64"); ?>
+	<?php  do_action("\x61\155\160\x5f\167\x70\x5f\x68\145\x61\x64"); ?>
 </head>
 <body <?php  body_class(); ?>>
 <?php  wp_body_open();
