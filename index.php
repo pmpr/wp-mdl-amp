ae148b63e73fd65c8eba67b61271d7adf8bdea0b
Pmnmpr <pmnmpr@gmail.com>
2022-03-09 11:09:35 +0000

    Obfuscated

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php
 /*   _______________________________________
     |  Obfuscated by PMPR - Php Obfuscator  |
-    |             620aa5f5dd44d             |
+    |             62288acf64dc8             |
     |_______________________________________|
 */
- use Pmpr\Package\AMP\AMP; AMP::symcgieuakksimmu(); if (!function_exists("\x69\163\x5f\x61\x6d\160")) { function is_amp() { return AMP::symcgieuakksimmu()->oywyqcgumoecwoga(); } } if (!function_exists("\151\163\137\141\155\160\137\145\x6e\x64\x70\157\151\156\164")) { function is_amp_endpoint() { return AMP::symcgieuakksimmu()->smowememmgeukwki(); } }
+ use Pmpr\Package\AMP\AMP; AMP::symcgieuakksimmu(); if (!function_exists("\151\x73\137\x61\x6d\x70")) { function is_amp() { return AMP::symcgieuakksimmu()->oywyqcgumoecwoga(); } } if (!function_exists("\151\163\x5f\x61\x6d\160\137\145\156\x64\x70\x6f\x69\156\164")) { function is_amp_endpoint() { return AMP::symcgieuakksimmu()->smowememmgeukwki(); } }
