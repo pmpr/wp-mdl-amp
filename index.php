a3eea872355600cc1cf0fbd5437125dd1abdddec
Pmnmpr <pmnmpr@gmail.com>
2022-03-27 06:20:35 +0000

    Obfuscated

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php
 /*   _______________________________________
     |  Obfuscated by PMPR - Php Obfuscator  |
-    |             623bb9b0c50f0             |
+    |             6240020a96880             |
     |_______________________________________|
 */
- use Pmpr\Package\AMP\AMP; AMP::symcgieuakksimmu(); if (!function_exists("\x69\x73\137\x61\x6d\x70")) { function is_amp() { return AMP::symcgieuakksimmu()->oywyqcgumoecwoga(); } } if (!function_exists("\151\163\137\x61\x6d\x70\x5f\145\156\x64\160\157\x69\x6e\x74")) { function is_amp_endpoint() { return AMP::symcgieuakksimmu()->smowememmgeukwki(); } }
+ use Pmpr\Package\AMP\AMP; AMP::symcgieuakksimmu(); if (!function_exists("\151\x73\x5f\x61\x6d\160")) { function is_amp() { return AMP::symcgieuakksimmu()->oywyqcgumoecwoga(); } } if (!function_exists("\151\x73\137\x61\155\160\x5f\145\x6e\144\x70\157\151\156\x74")) { function is_amp_endpoint() { return AMP::symcgieuakksimmu()->smowememmgeukwki(); } }
--- template/php/header.php
+++ template/php/header.php
@@ -1,16 +1,16 @@
 <?php
 /*   _______________________________________
     |  Obfuscated by PMPR - Php Obfuscator  |
-    |             623bb9b0c50f0             |
+    |             6240020a96880             |
     |_______________________________________|
 */
 ype html>
 <html amp ⚡ <?php  language_attributes(); ?>>
 <head>
-	<meta charset="<?php  bloginfo("\143\x68\x61\162\163\x65\x74"); ?>" />
+	<meta charset="<?php  bloginfo("\x63\x68\141\x72\x73\145\164"); ?>" />
 	<meta name="viewport" content="width=device-width, initial-scale=1" />
 	<link rel="profile" href="https://gmpg.org/xfn/11" />
-	<?php  do_action("\141\x6d\x70\137\167\160\137\x68\145\141\x64"); ?>
+	<?php  do_action("\x61\x6d\160\137\167\x70\137\150\x65\x61\x64"); ?>
 </head>
 <body <?php  body_class(); ?>>
-<?php  pmpr_do_action("\141\x6d\160\x5f\142\x6f\144\x79\x5f\157\160\145\156");
+<?php  pmpr_do_action("\x61\155\160\x5f\x62\x6f\x64\x79\137\157\160\x65\x6e");
