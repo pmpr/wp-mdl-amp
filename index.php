473a0bcc24f5b14a9118b6a4b82260b76b133cf9
Pmnmpr <pmnmpr@gmail.com>
2022-03-27 17:41:06 +0000

    Obfuscated

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php
 /*   _______________________________________
     |  Obfuscated by PMPR - Php Obfuscator  |
-    |             6240020a96880             |
+    |             6240a18c7f2cd             |
     |_______________________________________|
 */
- use Pmpr\Package\AMP\AMP; AMP::symcgieuakksimmu(); if (!function_exists("\151\x73\x5f\x61\x6d\160")) { function is_amp() { return AMP::symcgieuakksimmu()->oywyqcgumoecwoga(); } } if (!function_exists("\151\x73\137\x61\155\160\x5f\145\x6e\144\x70\157\151\156\x74")) { function is_amp_endpoint() { return AMP::symcgieuakksimmu()->smowememmgeukwki(); } }
+ use Pmpr\Module\AMP\AMP; AMP::symcgieuakksimmu(); if (!function_exists("\x69\x73\137\141\155\160")) { function is_amp() { return AMP::symcgieuakksimmu()->oywyqcgumoecwoga(); } } if (!function_exists("\x69\163\137\141\155\160\x5f\145\x6e\x64\160\x6f\x69\x6e\x74")) { function is_amp_endpoint() { return AMP::symcgieuakksimmu()->smowememmgeukwki(); } }
--- template/php/header.php
+++ template/php/header.php
@@ -1,16 +1,16 @@
 <?php
 /*   _______________________________________
     |  Obfuscated by PMPR - Php Obfuscator  |
-    |             6240020a96880             |
+    |             6240a18c7f2cd             |
     |_______________________________________|
 */
 ype html>
 <html amp ⚡ <?php  language_attributes(); ?>>
 <head>
-	<meta charset="<?php  bloginfo("\x63\x68\141\x72\x73\145\164"); ?>" />
+	<meta charset="<?php  bloginfo("\x63\x68\x61\x72\163\x65\164"); ?>" />
 	<meta name="viewport" content="width=device-width, initial-scale=1" />
 	<link rel="profile" href="https://gmpg.org/xfn/11" />
-	<?php  do_action("\x61\x6d\160\137\167\x70\137\150\x65\x61\x64"); ?>
+	<?php  do_action("\141\155\160\x5f\167\160\137\150\x65\141\x64"); ?>
 </head>
 <body <?php  body_class(); ?>>
-<?php  pmpr_do_action("\x61\155\160\x5f\x62\x6f\x64\x79\137\157\160\x65\x6e");
+<?php  pmpr_do_action("\141\155\x70\137\142\x6f\x64\x79\137\157\160\145\156");
