ba94e53c0f5681adaab64241de36ecc46829a282
Pmnmpr <pmnmpr@gmail.com>
2022-04-05 08:15:56 +0000

    Obfuscated

--- template/php/header.php
+++ template/php/header.php
@@ -1,16 +1,16 @@
 <?php
 /*   _______________________________________
     |  Obfuscated by PMPR - Php Obfuscator  |
-    |             6241d3964969c             |
+    |             624bfa942683f             |
     |_______________________________________|
 */
 ype html>
 <html amp ⚡ <?php  language_attributes(); ?>>
 <head>
-	<meta charset="<?php  bloginfo("\143\x68\141\x72\163\145\164"); ?>" />
+	<meta charset="<?php  bloginfo("\x63\150\x61\162\163\x65\164"); ?>" />
 	<meta name="viewport" content="width=device-width, initial-scale=1" />
 	<link rel="profile" href="https://gmpg.org/xfn/11" />
-	<?php  do_action("\141\x6d\160\137\167\160\137\150\145\x61\x64"); ?>
+	<?php  do_action("\x61\x6d\x70\x5f\167\160\x5f\150\x65\x61\x64"); ?>
 </head>
 <body <?php  body_class(); ?>>
-<?php  pmpr_do_action("\x61\x6d\x70\x5f\x62\x6f\144\x79\x5f\157\x70\145\x6e");
+<?php  pmpr_do_action("\141\x6d\160\137\x62\157\x64\171\x5f\157\160\x65\156");
