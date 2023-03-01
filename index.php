0b44f271501447e080397a73df73764556530c8d
Pmnmpr <pmnmpr@gmail.com>
2023-03-01 20:59:21 +0000

    Obfuscated

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php
 /*   _______________________________________
     |  Obfuscated by PMPR - Php Obfuscator  |
-    |             63fe3c5c3aa1d             |
+    |             63ffbc898cf9b             |
     |_______________________________________|
 */
- use Pmpr\Module\AMP\AMP; AMP::symcgieuakksimmu(); if (!function_exists("\x69\x73\137\x61\x6d\x70")) { function is_amp() { return AMP::symcgieuakksimmu()->oywyqcgumoecwoga(); } } if (!function_exists("\x69\x73\137\x61\x6d\x70\137\145\156\144\x70\157\x69\156\164")) { function is_amp_endpoint() { return AMP::symcgieuakksimmu()->smowememmgeukwki(); } }
+ use Pmpr\Module\AMP\AMP; AMP::symcgieuakksimmu(); if (!function_exists("\x69\x73\x5f\x61\x6d\x70")) { function is_amp() { return AMP::symcgieuakksimmu()->oywyqcgumoecwoga(); } } if (!function_exists("\x69\x73\137\141\x6d\160\x5f\145\x6e\144\x70\157\151\156\x74")) { function is_amp_endpoint() { return AMP::symcgieuakksimmu()->smowememmgeukwki(); } }
--- template/php/header.php
+++ template/php/header.php
@@ -1,16 +1,16 @@
 <?php
 /*   _______________________________________
     |  Obfuscated by PMPR - Php Obfuscator  |
-    |             63fe3c5c3aa1d             |
+    |             63ffbc898cf9b             |
     |_______________________________________|
 */
 ype html>
 <html amp ⚡ <?php  language_attributes(); ?>>
 <head>
-	<meta charset="<?php  bloginfo("\x63\x68\141\162\x73\145\x74"); ?>" />
+	<meta charset="<?php  bloginfo("\143\150\x61\162\163\145\x74"); ?>" />
 	<meta name="viewport" content="width=device-width, initial-scale=1" />
 	<link rel="profile" href="https://gmpg.org/xfn/11" />
-	<?php  do_action("\x61\x6d\x70\x5f\x77\x70\137\x68\x65\141\x64"); ?>
+	<?php  do_action("\141\155\x70\x5f\x77\160\x5f\x68\145\x61\x64"); ?>
 </head>
 <body <?php  body_class(); ?>>
-<?php  pmpr_do_action("\141\155\x70\137\x62\x6f\x64\171\x5f\x6f\160\x65\x6e");
+<?php  pmpr_do_action("\141\x6d\160\x5f\142\157\144\171\x5f\x6f\x70\x65\156");
