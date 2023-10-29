cc4e55c6135092a5b4288486ef2ac43df38cc051
pmpr.dev <pmpr.dev@gmail.com>
2023-10-29 14:33:29 +0000

    Obfuscated

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php
 /*   _______________________________________
     |  Obfuscated by PMPR - Php Obfuscator  |
-    |             65195d0f9aea3             |
+    |             653e6d156ac41             |
     |_______________________________________|
 */
- use Pmpr\Module\AMP\AMP; AMP::symcgieuakksimmu(); if (!function_exists("\151\163\x5f\x61\x6d\x70")) { function is_amp() { return AMP::symcgieuakksimmu()->oywyqcgumoecwoga(); } } if (!function_exists("\x69\163\137\141\x6d\160\x5f\145\x6e\x64\160\x6f\151\x6e\x74")) { function is_amp_endpoint() { return AMP::symcgieuakksimmu()->smowememmgeukwki(); } }
+ use Pmpr\Module\AMP\AMP; AMP::symcgieuakksimmu(); if (!function_exists("\151\163\x5f\x61\x6d\x70")) { function is_amp() { return AMP::symcgieuakksimmu()->oywyqcgumoecwoga(); } } if (!function_exists("\x69\163\137\x61\155\160\137\x65\156\x64\x70\x6f\151\156\x74")) { function is_amp_endpoint() { return AMP::symcgieuakksimmu()->smowememmgeukwki(); } }
--- template/php/header.php
+++ template/php/header.php
@@ -1,16 +1,16 @@
 <?php
 /*   _______________________________________
     |  Obfuscated by PMPR - Php Obfuscator  |
-    |             65195d0f9aea3             |
+    |             653e6d156ac41             |
     |_______________________________________|
 */
 ype html>
 <html amp ⚡ <?php  language_attributes(); ?>>
 <head>
-	<meta charset="<?php  bloginfo("\143\150\x61\x72\163\145\x74"); ?>" />
+	<meta charset="<?php  bloginfo("\x63\150\141\x72\163\x65\x74"); ?>" />
 	<meta name="viewport" content="width=device-width, initial-scale=1" />
 	<link rel="profile" href="https://gmpg.org/xfn/11" />
-	<?php  do_action("\x61\x6d\160\137\167\x70\137\x68\x65\141\144"); ?>
+	<?php  do_action("\x61\155\160\x5f\x77\x70\x5f\150\x65\x61\x64"); ?>
 </head>
 <body <?php  body_class(); ?>>
-<?php  pmpr_do_action("\141\155\x70\x5f\x62\157\x64\171\x5f\x6f\x70\145\156");
+<?php  pmpr_do_action("\x61\x6d\160\x5f\x62\x6f\144\x79\137\157\x70\145\x6e");
