b7ffc32a56a965d2651411c2a2c5103195f60c28
pmpr.dev <pmpr.dev@gmail.com>
2024-04-16 09:13:46 +0000

    Obfuscated

--- src/Pmpr/Module/AMP/Container.php
+++ src/Pmpr/Module/AMP/Container.php
@@ -1,7 +1,7 @@
 <?php
 /*   _______________________________________
     |  Obfuscated by PMPR - Php Obfuscator  |
-    |             660691ee9d24a             |
+    |             661e4124e9bcb             |
     |_______________________________________|
 */
- namespace Pmpr\Module\AMP; use Pmpr\Common\Foundation\Container\Container as BaseClass; use Pmpr\Module\AMP\Traits\CommonTrait; abstract class Container extends BaseClass { const qgwkyemuiussyyoy = "\x30\56\61"; const wcwemgogyesywcww = "\x68\x74\x74\x70\163\x3a\x2f\57\143\144\x6e\56\x61\x6d\x70\160\x72\157\x6a\x65\143\x74\56\157\162\147\57\x76\60"; use CommonTrait; public function __construct() { $this->settingObj = Setting::symcgieuakksimmu(); parent::__construct(); } }
+ namespace Pmpr\Module\AMP; use Pmpr\Common\Foundation\Container\Container as BaseClass; use Pmpr\Module\AMP\Traits\CommonTrait; abstract class Container extends BaseClass { const qgwkyemuiussyyoy = "\x30\56\x31"; const wcwemgogyesywcww = "\150\164\164\160\163\72\x2f\x2f\x63\144\156\56\141\x6d\x70\160\162\157\152\x65\x63\x74\56\x6f\x72\x67\x2f\x76\x30"; use CommonTrait; public function __construct() { $this->settingObj = Setting::symcgieuakksimmu(); parent::__construct(); } }
--- template/php/header.php
+++ template/php/header.php
@@ -1,16 +1,16 @@
 <?php
 /*   _______________________________________
     |  Obfuscated by PMPR - Php Obfuscator  |
-    |             660691ee9d24a             |
+    |             661e4124e9bcb             |
     |_______________________________________|
 */
 ype html>
 <html amp ⚡ <?php  language_attributes(); ?>>
 <head>
-	<meta charset="<?php  bloginfo("\x63\x68\141\x72\163\x65\164"); ?>" />
+	<meta charset="<?php  bloginfo("\143\x68\x61\x72\163\x65\164"); ?>" />
 	<meta name="viewport" content="width=device-width, initial-scale=1" />
 	<link rel="profile" href="https://gmpg.org/xfn/11" />
-	<?php  do_action("\141\x6d\x70\137\x77\160\x5f\150\x65\141\x64"); ?>
+	<?php  do_action("\141\155\x70\137\167\x70\137\150\145\x61\144"); ?>
 </head>
 <body <?php  body_class(); ?>>
-<?php  pmpr_do_action("\141\x6d\x70\137\x62\157\144\171\137\157\160\145\156");
+<?php  pmpr_do_action("\x61\x6d\x70\x5f\142\157\144\171\137\157\160\x65\x6e");
