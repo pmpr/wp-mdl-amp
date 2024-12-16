4085d7cb5fcb98949b95f572c06cd57e03511565
pmpr.dev <pmpr.dev@gmail.com>
2024-12-16 04:25:24 +0000

    Obfuscated

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php
 /*   _______________________________________
     |  Obfuscated by PMPR - Php Obfuscator  |
-    |             6757f0d6ac6a0             |
+    |             675fab92bf512             |
     |_______________________________________|
 */
- use Pmpr\Module\AMP\AMP; AMP::symcgieuakksimmu(); if (!function_exists("\x69\x73\x5f\x61\155\160")) { function is_amp() { return AMP::symcgieuakksimmu()->oywyqcgumoecwoga(); } } if (!function_exists("\151\x73\x5f\x61\x6d\x70\x5f\x65\x6e\x64\x70\157\151\156\164")) { function is_amp_endpoint() { return AMP::symcgieuakksimmu()->smowememmgeukwki(); } }
+ use Pmpr\Module\AMP\AMP; AMP::symcgieuakksimmu(); if (!function_exists("\x69\x73\137\x61\x6d\x70")) { function is_amp() { return AMP::symcgieuakksimmu()->oywyqcgumoecwoga(); } } if (!function_exists("\x69\x73\x5f\x61\155\x70\x5f\145\156\144\160\157\x69\x6e\x74")) { function is_amp_endpoint() { return AMP::symcgieuakksimmu()->smowememmgeukwki(); } }
--- src/Pmpr/Module/AMP/Container.php
+++ src/Pmpr/Module/AMP/Container.php
@@ -1,7 +1,7 @@
 <?php
 /*   _______________________________________
     |  Obfuscated by PMPR - Php Obfuscator  |
-    |             6757f0d6ac6a0             |
+    |             675fab92bf512             |
     |_______________________________________|
 */
- namespace Pmpr\Module\AMP; use Pmpr\Common\Foundation\Container\Container as BaseClass; use Pmpr\Module\AMP\Traits\CommonTrait; abstract class Container extends BaseClass { const qgwkyemuiussyyoy = "\60\x2e\x31"; const wcwemgogyesywcww = "\150\x74\164\x70\163\x3a\57\57\143\144\156\x2e\x61\155\160\160\162\x6f\x6a\145\143\164\x2e\157\x72\x67\57\x76\60"; use CommonTrait; }
+ namespace Pmpr\Module\AMP; use Pmpr\Common\Foundation\Container\Container as BaseClass; use Pmpr\Module\AMP\Traits\CommonTrait; abstract class Container extends BaseClass { const qgwkyemuiussyyoy = "\x30\x2e\61"; const wcwemgogyesywcww = "\x68\x74\x74\160\163\x3a\x2f\x2f\x63\144\156\x2e\x61\x6d\x70\x70\x72\x6f\152\x65\143\x74\x2e\x6f\162\x67\x2f\x76\60"; use CommonTrait; }
